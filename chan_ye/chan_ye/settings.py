483f0c11a4042738266bd20e0350647033108ba1
mengguiyouziyi <775618369@qq.com>
2017-12-05 17:26:09 +0800

    jqr_product

--- chan_ye/chan_ye/settings.py
+++ chan_ye/chan_ye/settings.py
@@ -37,7 +37,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -115,8 +115,8 @@ USER_AGENT_CHOICES = [
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-	'chan_ye.middlewares.ProxyMiddleware': 1,
-	'chan_ye.middlewares.RetryMiddleware': 110,
+	# 'chan_ye.middlewares.ProxyMiddleware': 1,
+	# 'chan_ye.middlewares.RetryMiddleware': 110,
 	'chan_ye.middlewares.RotateUserAgentMiddleware': 3,
 	# 'chan_ye.middlewares.BloomfilterMiddleware': 2,
 	# 'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': None,
